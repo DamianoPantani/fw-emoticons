4ffcbe8a845c1f3199b9441430a0503a94fb85be
szafulskid <D.szafulski@tt.com.pl>
2017-02-07 14:44:35 +0100

    Added many missing emoticons to default replacement map

--- dist/fw_emoticons.js
+++ dist/fw_emoticons.js
@@ -4,18 +4,19 @@ this.Emoticons = (function() {
 function Emoticons() {}
 
 Emoticons.prototype.defaultEmoMap = {
-'smile': [':)',':}',':]'],
-'wink': [';)',';}',';]'],
-'happy': [':D',';D'],
-'sad': [':('],
-'glasses': ['8)','8]','8}'],
-'happytongue': [':P',';P'],
+'smile': [':)',':}',':]',':>',':-)',':-}',':-]',':->'],
+'wink': [';)',';}',';]',';-)',';-}',';-]',';->'],
+'happy': [':D',';D',':-D',';-D','xD','xd','XD'],
+'sad': [':(',':{',':[',':<',':-(',':-{',':-[',':-<'],
+'weird': [';(',';{',';[',';<',';-(',';-{',';-[',';-<'],
+'glasses': ['B-)','B-}','B-]','B->','8-)','8-}','8-]','8->'],
+'happytongue': [':P',':p',';P',';p',':-P',';-P',':-p',';-p'],
 'meh': [':|'],
-'dead': [':X'],
-'lol': ['o_O', 'O_o', 'o.O', 'O.o', ':lol:'],
-'shocked': [':O'],
-'thoughtful': [' :/'], // space protects replacement in links
-'thoughtful2': [':\\'],
+'dead': [':X',':x',';x',';X'],
+'lol': ['o_o','o_O','o_0','o.o','o.O','o.0','O_o','O_O','O_0','O.o','O.O','O.0','0_o','0_O','0_0','0.o','0.O','0.0',':lol:'],
+'shocked': [':O',':o',':-O',':-o',';O',';o',';-O',';-o'],
+'thoughtful': [';-/',':-/',';/',' :/'], // space protects replacement in links
+'thoughtful2': [':\\',':-\\',';\\',';-\\'],
 'nerd': [':nerd:'],
 'inlove': [':inlove:'],
 'angry': [':angry:'],
